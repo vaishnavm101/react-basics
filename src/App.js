6e18e35d976008773ee6abf99240097c49c52c29
vaishnavm101 <vaishnavm363@gmail.com>
2024-09-16 18:13:46 +0530

    Added redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useRef, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment } from './features/counter';
+import { produce } from 'immer';
+// import { decrement, increment } from './features/counter';
+// import { decrement, increment, incrementByValue } from './redux/actions';
 
 
 const App = () => {
-    const { count } = useSelector((state) => state)
+    const { count, name } = useSelector((state) => state.counter)
+    const dispatch = useDispatch();
+    const inputRef = useRef();
+
+    let [obj, setObj] = useState({ name: "TestName", marks: [80, 90, 87], city: "TestCity" })
+
+    const handleClick = () => {
+        setObj({ ...obj, city: "New CIty" })
+        const newObj = produce(obj, (draft) => {
+            draft.city = "New City"
+        })
+        setObj(newObj)
+    }
+
     return (
         <>
             <h1>App</h1>
             <h2>Count: {count}</h2>
+            <h3>Name: {name}</h3>
+            <button onClick={() => dispatch(increment())}>Increment</button>
+            <button onClick={() => dispatch(decrement())}>Decrement</button>
+            <hr />
+
+            <h3>{JSON.stringify(obj)}</h3>
+            <button onClick={handleClick}>Change City</button>
+
+            {/* <button onClick={() => console.log(increment(100))}>Check</button> */}
+            {/* <h2>Users: {users}</h2> */}
+
+
+            {/* <hr />
+            <input type="text" ref={inputRef} placeholder='Enter User name' />
+            <button onClick={() => dispatch({ type: "users/add_user", payload: inputRef.current.value })}>Add User</button> */}
+            {/* 
+            <button onClick={handleIncByValue}>Increment by Value</button> */}
         </>
     )
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,59 @@
-import { createStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore, createStore } from "@reduxjs/toolkit"
+import { DECREMENT, INCREMENT, INCREMENT_BY_VALUE } from "./actions";
+// import { counterReducer } from "../features/counter";
+import counterReducer from "../features/counter";
+
+// const initialState = { count: 100 }
+
+// const counterReducer = (state = initialState, action) => {
+//     if (action.type === INCREMENT) {
+//         // Increment code....
+//         //  console.log(state);
+//         // console.log("Increment action is dipatched!!");
+//         return { count: state.count + 1 }
+//     }
+//     else if (action.type === DECREMENT) {
+//         // Decrement code....
+//         return { count: state.count - 1 }
+//     }
+//     else if (action.type === INCREMENT_BY_VALUE) {
+//         return { count: state.count + action.payload }
+//     }
+//     else {
+//         return state;
+//     }
+// }
+
+// const defaultUsers = ["User 1", "User 2"]
+// const usersReducer = (state = defaultUsers, action) => {
+//     switch (action.type) {
+//         case "users/add_user": {
+//             return [...state, action.payload]
+//         }
+//         default: {
+//             return state
+//         }
+//     }
+// }
 
 
-const defaultState = { count: 100 }
-const reducer = (state = defaultState, action) => {
-    if (action.type == "increment") {
-        return { count: state.count + 1 }
+// const rootReducer = combineReducers({
+//     counter: counterReducer,
+//     users: usersReducer
+// })
+
+// export const store = createStore(rootReducer)
+
+// export const store = configureStore({
+//     reducer: {
+//         counter: counterReducer,
+//         users: usersReducer
+//     }
+// })
+
+
+export const store = configureStore({
+    reducer: {
+        counter: counterReducer
     }
-    else{
-        return state;
-    }
-}
-
-export const store = createStore(reducer)
+})
